6bff8b0331dd3b5024ecb15670973d91365b1bb3
mshibl <mohamed.shibl@hotmail.com>
2016-03-06 16:24:44 -0800

    first commit

---
+++ tutorial1.js
@@ -0,0 +1,13 @@
+var CommentBox = React.createClass({
+	render: function(){
+		return (
+			<div className="commentBox">
+				Hello, world! I am a commentBox
+			</div>
+			);
+	}
+})
+ReactDOM.render(
+	<CommentBox />,
+	document.getElementById('contento')
+);
